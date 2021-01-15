eeed31b4468c4b519a2044d2712208fb2ca90633
namantiwari2002 <namantiwari2002@gmail.com>
2021-01-15 15:52:56 +0530

    cp practice first

---
+++ rotate_array.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void rotateArr(int arr[], int d, int n){
+    
+}
+
+
+
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	//taking testcases
+	cin >> t;
+	
+	while(t--){
+	    int n, d;
+	    
+	    //input n and d
+	    cin >> n >> d;
+	    
+	    int arr[n];
+	    
+	    //inserting elements in the array
+	    for(int i = 0; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    
+	    //calling rotateArr() function
+	    rotateArr(arr, d,n);
+	    
+	    //printing the elements of the array
+	    for(int i =0;i<n;i++){
+	        cout << arr[i] << " ";
+	    }
+	    cout << endl;
+	}
+	return 0;
+}  
