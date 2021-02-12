3f76535551eb4198cbd5e7ce8d2bba29579e7fdb
namantiwari2002 <namantiwari2002@gmail.com>
2021-02-12 15:43:49 +0530

    reverse linked list

---
+++ algos/reverse_linkedlist.cpp
@@ -0,0 +1,105 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+/* Function to get the middle of the linked list*/
+struct Node *reverseList(struct Node *head);
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+       printf("%d ", temp->data);
+       temp  = temp->next;
+    }
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n,l,firstdata;
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        
+        for (int i=1; i<n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        
+        head = reverseList(head);
+        
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+// Should reverse list and return new head.
+struct Node* reverseList(struct Node *head)
+{
+    
+    if(head->next == NULL){
+        return head;
+    }
+    
+    int count = 0;
+    
+    Node *n , *p , *temp;
+    p = head;
+    n = head->next;
+    
+    while(n != NULL){
+       
+       temp = n->next;
+       n->next = p;
+       p = n;
+       n = temp;
+       
+    }
+    
+    head->next = NULL;
+    
+    return p;
+    
+    
+    
+}
