c4b32e609f1c155c99497fb67a430031d3645a13
namantiwari2002 <namantiwari2002@gmail.com>
2021-02-13 13:56:22 +0530

    ll palindrome

---
+++ linked_list_palindrome.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+bool isPalindrome(Node *head);
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,firstdata;
+    cin>>T;
+    while(T--)
+    {
+        
+        struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        // taking first data of LL
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+   	cout<<isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+/*You are required to complete this method */
+bool isPalindrome(Node *head)
+{
+    
+    Node *a;
+    int num = 0 , k = 0;
+    a = head;
+    
+    while(a != NULL){
+        num = num*10 + a->data;
+        a = a->next;
+        
+    }
+    
+    int l = num;
+    
+    while(num >0){
+        
+        k = k*10 + num%10;
+        num = num/10;
+        
+    }
+    
+    if(l == k){
+        return true;
+    }else{
+        return false;
+    }
+    
+    
+    
+}
